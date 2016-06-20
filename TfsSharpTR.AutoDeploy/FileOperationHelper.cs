ee3a1843be5e34127c31cba599f19193f8036440
SonizciTR <ozan.uysal@msn.com>
2016-06-20 13:25:59 +0300

    Long path problem for Directory and File resolved

--- TfsSharpTR.AutoDeploy/FileOperationHelper.cs
+++ TfsSharpTR.AutoDeploy/FileOperationHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,11 +9,28 @@ using System.Threading.Tasks;
 
 namespace TfsSharpTR.AutoDeploy
 {
+    static class Win32Native
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public class SECURITY_ATTRIBUTES
+        {
+            public int nLength;
+            public IntPtr pSecurityDescriptor;
+            public int bInheritHandle;
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool CreateDirectory(string lpPathName, SECURITY_ATTRIBUTES lpSecurityAttributes);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern SafeFileHandle CreateFile(string lpFileName, int dwDesiredAccess, FileShare dwShareMode, SECURITY_ATTRIBUTES securityAttrs, FileMode dwCreationDisposition, int dwFlagsAndAttributes, IntPtr hTemplateFile);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        public static extern bool CopyFile(string lpExistingFileName, string lpNewFileName, bool bFailIfExists);
+    }
+
     internal static class FileOperationHelper
     {
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
-        private static extern bool CopyFile(string lpExistingFileName, string lpNewFileName, bool bFailIfExists);
-
         public static string TakeBackup(AutoDeploySettingItem setting, string sourceFolder, string backupFolder)
         {
             string nBckupFolder = setting.GetBackupFolder(sourceFolder);
@@ -24,49 +42,59 @@ namespace TfsSharpTR.AutoDeploy
 
         public static string DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs, bool overwrite = true)
         {
-            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
-            DirectoryInfo[] dirs = dir.GetDirectories();
-
-            // If the source directory does not exist, throw an exception.
-            if (!dir.Exists)
+            try
             {
-                throw new DirectoryNotFoundException(
-                    "Source directory does not exist or could not be found: "
-                    + sourceDirName);
-            }
+                DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+                DirectoryInfo[] dirs = dir.GetDirectories();
 
-            // If the destination directory does not exist, create it.
-            if (!Directory.Exists(destDirName))
-            {
-                Directory.CreateDirectory(destDirName);
-            }
-
-
-            // Get the file contents of the directory to copy.
-            FileInfo[] files = dir.GetFiles();
-
-            foreach (FileInfo file in files)
-            {
-                // Create the path to the new copy of the file.
-                string temppath = Path.Combine(destDirName, file.Name);
-
-                // Copy the file. Long file path problem
-                //file.CopyTo(temppath, overwrite);
-                CopyFile(file.FullName, temppath, !overwrite);
-            }
-
-            // If copySubDirs is true, copy the subdirectories.
-            if (copySubDirs)
-            {
-
-                foreach (DirectoryInfo subdir in dirs)
+                // If the source directory does not exist, throw an exception.
+                if (!dir.Exists)
                 {
-                    // Create the subdirectory.
-                    string temppath = Path.Combine(destDirName, subdir.Name);
-
-                    // Copy the subdirectories.
-                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                    throw new DirectoryNotFoundException(
+                        "Source directory does not exist or could not be found: "
+                        + sourceDirName);
                 }
+
+                // If the destination directory does not exist, create it.
+                if (!Directory.Exists(destDirName))
+                {
+                    //Directory.CreateDirectory(destDirName);
+                    var isCrtd = Win32Native.CreateDirectory(String.Concat(@"\\?\", destDirName), null);
+                    if (!isCrtd)
+                        return "Directory could not created. Path = " + destDirName;
+                }
+
+
+                // Get the file contents of the directory to copy.
+                FileInfo[] files = dir.GetFiles();
+
+                foreach (FileInfo file in files)
+                {
+                    // Create the path to the new copy of the file.
+                    string temppath = Path.Combine(destDirName, file.Name);
+
+                    // Copy the file. Long file path problem
+                    //file.CopyTo(temppath, overwrite);
+                    Win32Native.CopyFile(file.FullName, temppath, !overwrite);
+                }
+
+                // If copySubDirs is true, copy the subdirectories.
+                if (copySubDirs)
+                {
+
+                    foreach (DirectoryInfo subdir in dirs)
+                    {
+                        // Create the subdirectory.
+                        string temppath = Path.Combine(destDirName, subdir.Name);
+
+                        // Copy the subdirectories.
+                        DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return "FileOperationHelper.DirectoryCopy failed. ErrDetail = " + ex.ToString();
             }
 
             return null;

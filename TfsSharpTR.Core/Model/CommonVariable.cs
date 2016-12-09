22c0bca974930bfa0f410170f855a134cd3d0f50
SonizciTR <ozan.uysal@msn.com>
2016-12-09 23:12:21 +0300

    mend

---
+++ TfsSharpTR.Core/Model/CommonVariable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TfsSharpTR.Core.Model
+{
+    /// <summary>
+    /// Common management of Tfs variables and User variables
+    /// </summary>
+    public class CommonVariable
+    {
+        public Dictionary<string, string> RawData { get; set; } = new Dictionary<string, string>();
+
+        public CommonVariable(Dictionary<string, string> keys)
+        {
+            RawData = keys;
+        }
+
+        internal string Get(string keyWord)
+        {
+            string tmp;
+
+            return RawData.TryGetValue(keyWord, out tmp) ? tmp : null;
+        }
+    }
+}
